142d3235abe4b8c23216093b69fc35695e7c2127
PProvost <peter@provost.org>
2009-07-06 12:14:56 -0600

    A bit more debugging on the new algoritm Switched to a FSM for the states of the system

--- ChannelLock.lua
+++ ChannelLock.lua
@@ -40,13 +40,11 @@ local knownTradeZones = {
 }
 
 function ChannelLock:OnInitialize()
-	self.defaults = self:GetDefaults()
-	self.db = LibStub("AceDB-3.0"):New("ChannelLockDB", self.defaults, "Default")
+	self.db = LibStub("AceDB-3.0"):New("ChannelLockDB", self:GetDefaults(), "Default")
 
 	LibStub("tekKonfig-AboutPanel").new(nil, "ChannelLock")
 
-	self.options = self:GetOptions()
-	LibStub("AceConfig-3.0"):RegisterOptionsTable("ChannelLock", self.options )
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("ChannelLock", self:GetOptions() )
 	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ChannelLock", "Channels", "ChannelLock", "channels")
 	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ChannelLock", "Profiles", "ChannelLock", "profiles")
 
@@ -63,12 +61,36 @@ function ChannelLock:OnEnable()
 	local channels = self.db.profile.channels
 	for i = 1,10 do
 		if channels[i].name == nil or channels[i].name == "" or channels[i].empty then
-			channels[i].name = ""
-			channels[i].empty = true
+			channels[i].name = ""; channels[i].empty = true
 		end
 	end
 
-	self:ScheduleTimer("CheckChannels", 1.0)
+	-- Kick it off
+	self.state = "CheckChannels"
+	self.stateTimer = self:ScheduleRepeatingTimer("StateMachine", 3.0)
+end
+
+function ChannelLock:StateMachine()
+	if self.state == "CheckChannels" then
+		self.state = (self:CheckChannels() and "SetupChannelFrames") or "DropAllChannels"
+	elseif self.state == "DropAllChannels" then
+		self:Print("Channels need adjusting. One moment...")
+		self:DropAllChannels()
+		self.state = "JoinAllChannels"
+	elseif self.state == "JoinAllChannels" then
+		self:JoinAllChannels()
+		self.state = "DropTempChannels"
+	elseif self.state == "DropTempChannels" then
+		self:DropTempChannels()
+		self.state = "SetupChannelFrames"
+	elseif self.state == "SetupChannelFrames" then
+		self:SetupChannelFrames()
+		self.state = "End"
+	else
+		self:CancelTimer(self.stateTimer)
+		self:Print("Channels setup correctly. Enjoy!")
+		self.state = "End"
+	end
 end
 
 function ChannelLock:IsTradeChannelAndNotTradeZone(channel)
@@ -106,13 +128,7 @@ end
 function ChannelLock:CheckChannels()
 	local goal = self.db.profile.channels
 	local source = self:GetSourceList()
-	if not self:IsChannelListCorrect(source,goal) then
-		self:Print("Your channels appear to need adjustment. One moment please.")
-		self:DropAllChannels()
-		self:ScheduleTimer("JoinAllChannels", 1.0)
-	else
-		self:Print("Your channels are set correctly. No adjustment required.")
-	end
+	return self:IsChannelListCorrect(source,goal)
 end
 
 function ChannelLock:JoinAllChannels()
@@ -124,7 +140,6 @@ function ChannelLock:JoinAllChannels()
 			self:JoinChannel(channels[i].name, channels[i].frameIndex)
 		end
 	end
-	self:ScheduleTimer("DropTempChannels", 2.0)
 end
 
 function ChannelLock:DropAllChannels()
@@ -138,7 +153,6 @@ function ChannelLock:DropTempChannels()
 	for i,name in ipairs(self.tempChannels) do
 		self:LeaveChannel(name)
 	end
-	self:ScheduleTimer("SetupChannelFrames", 2.0)
 end
 
 function ChannelLock:SetupChannelFrames()
@@ -150,7 +164,6 @@ function ChannelLock:SetupChannelFrames()
 		end
 	end
 		
-	self:Print("Channels now setup correctly. Enjoy!")
 end
 
 function ChannelLock:CompareChannelInfo( source, goal )
@@ -159,14 +172,12 @@ function ChannelLock:CompareChannelInfo( source, goal )
 end
 
 function ChannelLock:IsChannelListCorrect(source, goal)
-   local clean = true
    for i = 1,10 do
       if not self:CompareChannelInfo(source[i], goal[i]) then
-         clean = false
-         print (i)
-         break
+				return false
       end
    end
+	 return true
 end
 
 -- this reduces server zone channels (general, trade, etc to their base parts with no zone info)
@@ -183,8 +194,6 @@ function ChannelLock:JoinChannel(channel, frameIndex)
 	if not channel then return end
 	if not frameIndex then frameIndex = 1 end
 	JoinPermanentChannel(channel, nil, frameIndex, nil)
-	-- Damn this is too quick
-	ChatFrame_AddChannel(_G["ChatFrame"..frameIndex], channel)
 end
 
 function ChannelLock:JoinTempChannel(index)
